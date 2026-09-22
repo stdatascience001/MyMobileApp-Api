eac1c2ccf1d06e282286a3659a1012471ae7db68
Abhishek <st.datascience001@gmail.com>
2026-09-22 20:00:35 +0530

    update config commit

--- app/core/config.py
+++ app/core/config.py
@@ -1,11 +1,14 @@
+from pydantic_settings import SettingsConfigDict
 import os
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres:Sss1234!@localhost:5432/travel_companion")
-    SECRET_KEY: str = os.getenv("SECRET_KEY", "my-super-secret-jwt-key")
-    ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
-    REFRESH_TOKEN_EXPIRE_DAYS: int = int(os.getenv("REFRESH_TOKEN_EXPIRE_DAYS", "7"))
+    DATABASE_URL: str 
+    SECRET_KEY: str 
+    ALGORITHM: str =  "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+
+    model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings()
